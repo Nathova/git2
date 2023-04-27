fc473ae259d0ac4eee54e952de5260bbc34d0369
Екатерина Натхова <nathovaekaterina@gmail.com>
2023-04-27 21:19:45 +0300

    Create str_func.py

---
+++ str_func.py
@@ -0,0 +1,7 @@
+"""функция, делающая все буквы в строке на заглавные"""
+def lower(string):
+    return string.lower()
+
+"""функция, делающая заглавными первые 2 буквы"""
+def first_two_lower(string):
+    return (string[1,2].lower())
